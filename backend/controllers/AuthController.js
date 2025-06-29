4c7cbfbb66c6795d0a367a970d98d2c3f868091f
Priyanshurawatofficial <priyanshurawatmaster@gmail.com>
2025-06-29 21:44:40 +0530

    fixed cookies

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -19,8 +19,9 @@ module.exports.SignUp=async (req , res , next)=>{
         const token = createSecretToken(user._id);
      
         res.cookie("token", token, {
-      withCredentials: true,
-      httpOnly: false,
+      httpOnly: true,
+  sameSite: "none",
+  secure: true
     });
 
 
@@ -54,8 +55,9 @@ module.exports.Login=async(req,res,next)=>{
      }
     const token=createSecretToken(user._id);
       res.cookie("token", token, {
-       withCredentials: true,
-       httpOnly: false,
+       httpOnly: true,
+       sameSite: "none",
+       secure: true
      });
       res.status(201).json({ message: "User logged in successfully", success: true });
      next()
