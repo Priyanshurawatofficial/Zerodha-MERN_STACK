62137da6a3a9740c60fdd85616121d543d7b4d06
Priyanshurawatofficial <priyanshurawatmaster@gmail.com>
2025-06-29 21:14:34 +0530

    fixed hero image

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
     return ( 
    <div className='container p-10 mb-5'>
   <div className='row text-center'>
-       <img src='media/images/homehero.png' alt='heroimage.png' className='mb-5'/>
+       <img src='media/images/homeHero.png' alt='heroimage.png' className='mb-5'/>
        <h1 className=''><b>Invest in everything</b></h1>
        <p className='text-muted'>Online platform to invest in stocks,derivatives,mutual funds,and more</p>
       <a href='/signup'> <button className='btn btn-primary p-3 fs-5 mb-5' style={{width:"20%",margin:"auto",color:"white",backgroundColor:""}}><b>Sign up for free</b></button> </a>
